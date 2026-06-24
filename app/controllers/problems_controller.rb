845de4b39cee76a1b82a830b55769cd3ee600aef
kyulyak <yulia.kirkina@mail.ru>
2026-06-24 23:49:36 +0300

    last commit

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -10,9 +10,16 @@ class ProblemsController < ApplicationController
     @generated_problem = generated_problem_from_params
     submitted_answer = normalize_submitted_answer
 
+    if submitted_answer.blank?
+      @check_result = nil
+      render :show, status: :ok
+      return
+    end
+
     @check_result = Trainer::GeneratedAnswerChecker.new(
       correct_answer: @generated_problem["correct_answer"],
-      submitted_answer: submitted_answer
+      submitted_answer: submitted_answer,
+      problem_type: @problem.problem_type
     ).call
 
     track_attempt(submitted_answer) if current_user.present?
@@ -54,22 +61,28 @@ class ProblemsController < ApplicationController
 
   def normalize_submitted_answer
     if @generated_problem["answer_type"] == "matrix" || @generated_problem["answer_type"] == "matrix_with_factor"
+      return nil if params[:submitted_answer].blank?
+
       rows = params[:submitted_answer].to_unsafe_h.values
 
       matrix = rows.map do |row|
         row.values.map { |value| value.to_s.strip }
       end
 
+      return nil if matrix.flatten.any?(&:blank?)
+
       matrix_string = "[[#{matrix[0][0]},#{matrix[0][1]}],[#{matrix[1][0]},#{matrix[1][1]}]]"
 
       if @generated_problem["answer_type"] == "matrix_with_factor"
         factor = params[:submitted_factor].to_s.strip
+        return nil if factor.blank?
+
         "#{factor}*#{matrix_string}"
       else
         matrix_string
       end
     else
-      params[:submitted_answer]
+      params[:submitted_answer].to_s.strip
     end
   end
 
--- app/services/trainer/generated_answer_checker.rb
+++ app/services/trainer/generated_answer_checker.rb
@@ -2,14 +2,15 @@ module Trainer
   class GeneratedAnswerChecker
     EPSILON = 0.0001
 
-    def initialize(correct_answer:, submitted_answer:)
-      @correct_answer = correct_answer
-      @submitted_answer = submitted_answer
+    def initialize(correct_answer:, submitted_answer:, problem_type: nil)
+      @correct_answer = correct_answer.to_s.strip
+      @submitted_answer = submitted_answer.to_s.strip
+      @problem_type = problem_type.to_s
     end
 
     def call
       {
-        correct: mathematically_equal?(@submitted_answer, @correct_answer),
+        correct: correct?,
         submitted_answer: @submitted_answer,
         correct_answer: @correct_answer
       }
@@ -17,49 +18,126 @@ module Trainer
 
     private
 
-    def mathematically_equal?(submitted, correct)
-      x_values = [ 1, 2, 3, 5 ]
+    def correct?
+      case @problem_type
+      when "matrix_addition", "matrix_multiplication"
+        matrix_equal?
+      when "matrix_inverse"
+        matrix_with_factor_equal?
+      when "matrix_determinant", "definite_integral", "polynomial_limit", "substitution_limit"
+        numeric_equal?
+      when "indefinite_integral"
+        integral_equal?
+      else
+        expression_equal?
+      end
+    end
 
-      x_values.all? do |x|
-        submitted_value = evaluate(submitted, x)
-        correct_value = evaluate(correct, x)
+    def numeric_equal?
+      submitted = Dentaku::Calculator.new.evaluate!(normalized_math(@submitted_answer))
+      correct = Dentaku::Calculator.new.evaluate!(normalized_math(@correct_answer))
 
-        return false if submitted_value.nil? || correct_value.nil?
+      (submitted.to_f - correct.to_f).abs < EPSILON
+    rescue
+      false
+    end
 
-        (submitted_value - correct_value).abs < EPSILON
+    def expression_equal?
+      [ 1, 2, 3, 5 ].all? do |x|
+        submitted = Dentaku::Calculator.new.evaluate!(normalized_math(@submitted_answer), x: x)
+        correct = Dentaku::Calculator.new.evaluate!(normalized_math(@correct_answer), x: x)
+
+        (submitted.to_f - correct.to_f).abs < EPSILON
       end
     rescue
       false
     end
 
-    def evaluate(expression, x)
-      normalized = normalize(expression)
+    def integral_equal?
+      submitted_expr = normalize_integral(@submitted_answer)
+      correct_expr = normalize_integral(@correct_answer)
 
-      Rails.logger.info "RAW: #{expression}"
-      Rails.logger.info "NORMALIZED: #{normalized}"
-      Rails.logger.info "X: #{x}"
+      differences = [ 1, 2, 3, 5 ].map do |x|
+        submitted = Dentaku::Calculator.new.evaluate!(submitted_expr, x: x)
+        correct = Dentaku::Calculator.new.evaluate!(correct_expr, x: x)
 
-      calculator = Dentaku::Calculator.new
-      calculator.evaluate!(normalized, x: x)
+        submitted.to_f - correct.to_f
+      end
+
+      differences.all? do |difference|
+        (difference - differences.first).abs < EPSILON
+      end
+    rescue
+      false
     end
 
-    def normalize(value)
+    def matrix_equal?
+      normalize_matrix(@submitted_answer) == normalize_matrix(@correct_answer)
+    end
+
+    def matrix_with_factor_equal?
+      normalize_matrix_with_factor(@submitted_answer) == normalize_matrix_with_factor(@correct_answer)
+    end
+
+    def normalized_math(value)
       value.to_s
-       .downcase
-       .gsub("\\(", "")
-       .gsub("\\)", "")
-       .gsub("\\left", "")
-       .gsub("\\right", "")
-       .gsub("\\cdot", "*")
-       .gsub(",", ".")
-       .gsub("·", "*")
-       .gsub(/[{}]/, "")
-       .gsub(".", "")
-       .gsub(/\s+/, "")
-       .gsub(/(\d)(x)/, '\1*x')
-       .gsub(/(\d)(\()/, '\1*\2')
-       .gsub(/(x)(\()/, 'x*\2')
-       .gsub(/(\))(\d|x|\()/, '\1*\2')
+           .downcase
+           .gsub("\\(", "")
+           .gsub("\\)", "")
+           .gsub("\\left", "")
+           .gsub("\\right", "")
+           .gsub("\\cdot", "*")
+           .gsub("\\,", "")
+           .gsub(",", ".")
+           .gsub("·", "*")
+           .gsub("−", "-")
+           .gsub(/[{}]/, "")
+           .gsub(/\s+/, "")
+           .gsub(/(\d)(x)/, '\1*x')
+           .gsub(/x\^2/, "x*x")
+           .gsub(/x\^3/, "x*x*x")
+           .gsub(/x\^4/, "x*x*x*x")
+           .gsub(/x\^5/, "x*x*x*x*x")
+    end
+
+    def normalize_matrix(value)
+      value.to_s
+           .gsub(/\s+/, "")
+           .gsub("−", "-")
+    end
+
+    def normalize_matrix_with_factor(value)
+      value.to_s
+           .gsub(/\s+/, "")
+           .gsub("−", "-")
+           .gsub("(1/", "1/")
+           .gsub(")*", "*")
+    end
+
+    def normalize_integral(value)
+      value.to_s
+          .downcase
+          .gsub("с", "c") # русская буква с
+          .gsub("\\(", "")
+          .gsub("\\)", "")
+          .gsub("\\left", "")
+          .gsub("\\right", "")
+          .gsub("\\,", "")
+          .gsub("\\cdot", "*")
+          .gsub("·", "*")
+          .gsub("−", "-")
+          .gsub(",", ".")
+          .gsub(/\s+/, "")
+          .gsub(/([a-z])\^\{(\d+)\}/, '\1^\2')
+          .gsub(/\\frac\{([^{}]+)\}\{([^{}]+)\}/, '(\1)/(\2)')
+          .gsub(/[{}]/, "")
+          .gsub(/\+c$/, "")
+          .gsub(/-c$/, "")
+          .gsub(/(\d)(x)/, '\1*\2')
+          .gsub(/x\^2/, "x*x")
+          .gsub(/x\^3/, "x*x*x")
+          .gsub(/x\^4/, "x*x*x*x")
+          .gsub(/x\^5/, "x*x*x*x*x")
     end
   end
 end
